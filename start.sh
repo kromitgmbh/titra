5536ffd7f2182c2601cb59e9ad51e4107043b476
Fabian Kromer <fabian.kromer@gmail.com>
2017-05-23 15:36:22 +0200

    removed useless cloudron files

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eux
-export MONGO_URL="${MONGODB_URL}"
-export ROOT_URL="${APP_ORIGIN}"
-export PORT=3000
-exec /usr/local/bin/gosu cloudron:cloudron node /app/code/bundle/main.js
--- test_cloudron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm titra.tar.gz
-meteor build --architecture os.linux.x86_64 /Users/fabian/build/titra/
-cp /Users/fabian/build/titra/titra.tar.gz .
-cloudron build
-cloudron install
