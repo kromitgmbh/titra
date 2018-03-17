450a13f74e1212c515145b9059ae9fd96c71b54d
Fabian Kromer <fabian.kromer@gmail.com>
2018-03-17 17:21:44 +0100

    :bug: avoid duplicates in the new customer filter

--- imports/api/customers/server/publications.js
+++ imports/api/customers/server/publications.js
@@ -3,15 +3,20 @@ import Projects from '../../projects/projects.js'
 
 Meteor.publish('projectCustomers', function projectCustomers({ projectId }) {
   check(projectId, String)
+  const customers = []
   if (projectId === 'all') {
     Projects.find(
       { $or: [{ userId: this.userId }, { public: true }, { team: this.userId }] },
       { _id: 1 },
     ).forEach((item) => {
-      this.added('customers', Random.id(), { name: item.customer })
+      if (!customers.includes(item.customer)) {
+        this.added('customers', Random.id(), { name: item.customer })
+        customers.push(item.customer)
+      }
     })
-  } else {
+  } else if (!customers.includes(Projects.findOne({ _id: projectId }).customer)) {
     this.added('customers', Random.id(), { name: Projects.findOne({ _id: projectId }).customer })
+    customers.push(Projects.findOne({ _id: projectId }).customer)
   }
   this.ready()
 })
