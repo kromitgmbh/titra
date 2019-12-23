01d081c2086cfdb965e745d8b3701356cbff0957
Fabian Kromer <fabian.kromer@gmail.com>
2019-12-23 15:26:19 +0100

    ⬆️ updated Meteor to version 1.8.3 ⬆️ updated package dependencies ✨ introducing the new weekly time input mode 🔥 removing deprecated package dependencies

--- imports/ui/components/weektable.js
+++ imports/ui/components/weektable.js
@@ -1,4 +1,5 @@
 import moment from 'moment'
+import i18next from 'i18next'
 import { Mongo } from 'meteor/mongo'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import './weektable.html'
@@ -10,11 +11,11 @@ function getWeekDays(date) {
   return new Array(7).fill(0).map(() => (calendar.add(1, 'day').format('ddd, DD.MM')))
 }
 const clientTimecards = new Mongo.Collection('clientTimecards')
+
 Template.weektable.onCreated(function weekTableCreated() {
   this.subscribe('myprojects')
   this.startDate = new ReactiveVar(moment(moment().startOf('week')))
   this.endDate = new ReactiveVar(moment(moment().endOf('week')))
-
   this.autorun(() => {
     if (FlowRouter.getQueryParam('date')) {
       this.startDate.set(moment(moment(FlowRouter.getQueryParam('date')).startOf('week')), 'YYYY-MM-DD')
@@ -35,17 +36,48 @@ Template.weektable.helpers({
 })
 
 Template.weektable.events({
-  'click .js-previous': (event, templateInstance) => {
+  'click .js-previous-week': (event, templateInstance) => {
     event.preventDefault()
     FlowRouter.setQueryParams({ date: moment(templateInstance.startDate.get()).subtract(1, 'week').format('YYYY-MM-DD') })
   },
-  'click .js-next': (event, templateInstance) => {
+  'click .js-next-week': (event, templateInstance) => {
     event.preventDefault()
     FlowRouter.setQueryParams({ date: moment(templateInstance.startDate.get()).add(1, 'week').format('YYYY-MM-DD') })
+  },
+  'click .js-save': (event, templateInstance) => {
+    event.preventDefault()
+    const weekArray = []
+    $('.js-hours').each((index, element) => {
+      const startDate = templateInstance.startDate.get().clone().startOf('week')
+      const value = $(element).val()
+      if (value) {
+        const task = $(element).data('task') ? $(element).data('task') : $(element.parentElement.parentElement).find('.js-tasksearch-input').val()
+        let hours = Number(value)
+        if (Meteor.user().profile.timeunit === 'd') {
+          hours *= (Meteor.user().profile.hoursToDays ? Meteor.user().profile.hoursToDays : 8)
+        }
+        weekArray.push({
+          projectId: $(element).data('project-id'),
+          task,
+          date: moment.utc(startDate.add(Number($(element).data('week-day')) + 1, 'day').format('YYYY-MM-DD')).toDate(),
+          hours,
+        })
+      }
+    })
+    Meteor.call('upsertWeek', weekArray, (error) => {
+      if (error) {
+        console.error(error)
+      } else {
+        $('.js-tasksearch-input').val('')
+        $('.js-tasksearch-input').parent().parent().find('.js-hours').val('')
+        $.notify(i18next.t('notifications.time_entry_updated'))
+      }
+    })
   },
 })
 
 Template.weektablerow.onCreated(function weektablerowCreated() {
+  this.tempTimeEntries = new ReactiveVar([])
   this.autorun(() => {
     this.subscribe('userTimeCardsForPeriodByProjectByTask',
       {
@@ -54,6 +86,12 @@ Template.weektablerow.onCreated(function weektablerowCreated() {
         endDate: Template.instance().data.endDate.get().toDate(),
       })
   })
+})
+Template.weektablerow.events({
+  'click .js-newline': (event, templateInstance) => {
+    event.preventDefault()
+    templateInstance.tempTimeEntries.set(templateInstance.tempTimeEntries.get().push({ _id: '' }))
+  },
 })
 Template.weektablerow.helpers({
   weekDays() {
@@ -70,20 +108,67 @@ Template.weektablerow.helpers({
           },
         },
       },
-    )
+    ).fetch().concat(Template.instance().tempTimeEntries.get())
   },
   getHoursForDay(day, task) {
-    const entryForDay = task.entries
-      .find((entry) => moment(entry.date).format('ddd, DD.MM') === day)
-    if (!Meteor.loggingIn() && Meteor.user() && Meteor.user().profile && entryForDay) {
-      const precision = Meteor.user().profile.precision ? Meteor.user().profile.precision : 2
-      if (Meteor.user().profile.timeunit === 'd') {
-        const convertedTime = Number(entryForDay.hours / (Meteor.user().profile.hoursToDays
-          ? Meteor.user().profile.hoursToDays : 8)).toFixed(precision)
-        return convertedTime !== Number(0).toFixed(precision) ? convertedTime : undefined
+    if (task.entries) {
+      const entryForDay = task.entries
+        .find((entry) => moment(entry.date).format('ddd, DD.MM') === day)
+      if (!Meteor.loggingIn() && Meteor.user() && Meteor.user().profile && entryForDay) {
+        const precision = Meteor.user().profile.precision ? Meteor.user().profile.precision : 2
+        if (Meteor.user().profile.timeunit === 'd') {
+          const convertedTime = Number(entryForDay.hours / (Meteor.user().profile.hoursToDays
+            ? Meteor.user().profile.hoursToDays : 8)).toFixed(precision)
+          return convertedTime !== Number(0).toFixed(precision) ? convertedTime : undefined
+        }
+        return Number(entryForDay.hours).toFixed(precision)
       }
-      return Number(entryForDay.hours).toFixed(precision)
     }
     return ''
+  },
+  getTotalForTask(task) {
+    if (task.entries) {
+      if (!Meteor.loggingIn() && Meteor.user() && Meteor.user().profile) {
+        const total = task.entries
+          .reduce((tempTotal, amount) => tempTotal + Number(amount.hours), 0)
+        const precision = Meteor.user().profile.precision ? Meteor.user().profile.precision : 2
+        if (Meteor.user().profile.timeunit === 'd') {
+          const convertedTotal = Number(total / (Meteor.user().profile.hoursToDays
+            ? Meteor.user().profile.hoursToDays : 8)).toFixed(precision)
+          return convertedTotal !== Number(0).toFixed(precision) ? convertedTotal : undefined
+        }
+        return Number(total).toFixed(precision)
+      }
+    }
+    return ''
+  },
+  getTotalForDayPerProject(projectId, day) {
+    let total = 0
+    if (!Meteor.loggingIn() && Meteor.user() && Meteor.user().profile) {
+      clientTimecards.find(
+        {
+          entries:
+          {
+            $elemMatch:
+            {
+              projectId,
+            },
+          },
+        },
+      ).fetch().concat(Template.instance().tempTimeEntries.get()).forEach((element) => {
+        if (element.entries) {
+          total += element.entries.filter((entry) => moment(entry.date).format('ddd, DD.MM') === day)
+            .reduce((tempTotal, current) => tempTotal + Number(current.hours), 0)
+        }
+      })
+      const precision = Meteor.user().profile.precision ? Meteor.user().profile.precision : 2
+      if (Meteor.user().profile.timeunit === 'd') {
+        const convertedTotal = Number(total / (Meteor.user().profile.hoursToDays
+          ? Meteor.user().profile.hoursToDays : 8)).toFixed(precision)
+        return convertedTotal !== Number(0).toFixed(precision) ? convertedTotal : undefined
+      }
+      return Number(total).toFixed(precision)
+    }
+    return false
   },
 })
