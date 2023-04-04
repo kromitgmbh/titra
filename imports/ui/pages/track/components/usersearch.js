e103fd51fdd7f0cb086e2763dd1734dc32650ba9
Andreas Bomholtz <andreas@seluxit.com>
2023-04-04 08:52:45 +0200

    Use Autocomplete util to handle user search

--- imports/ui/pages/track/components/usersearch.js
+++ imports/ui/pages/track/components/usersearch.js
@@ -1,71 +1,16 @@
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra'
 import './usersearch.html'
 import Timecards from '../../../../api/timecards/timecards.js'
+import Autocomplete from '../../../../utils/autocomplete'
+import { getGlobalSetting } from '../../../../utils/frontend_helpers'
 
 Template.usersearch.events({
   'click .js-remove-value': (event, templateInstance) => {
     event.preventDefault()
     event.stopPropagation()
     templateInstance.$('.js-usersearch-input').val('')
-  },
-  'mousedown .js-usersearch-result': (event, templateInstance) => {
-    event.preventDefault()
-    templateInstance.$('.js-usersearch-input').val(templateInstance.$(event.currentTarget).children('.js-usersearch-user-name').text())
-    templateInstance.$('.js-usersearch-results').addClass('d-none')
-    templateInstance.$('.js-usersearch-input').removeClass('is-invalid')
-    if ($('#tasks')) {
-      $('#tasks').focus()
-    }
-  },
-  'focus .js-usersearch-input': (event, templateInstance) => {
-    templateInstance.$('.js-usersearch-results').removeClass('d-none')
-    templateInstance.$('.js-usersearch-input').removeClass('is-invalid')
-  },
-  'blur .js-usersearch-input': (event, templateInstance) => {
-    if (!event.relatedTarget || $(event.relatedTarget).is($('.js-target-project').first())) {
-      templateInstance.$('.js-usersearch-results').addClass('d-none')
-    }
-  },
-  'keydown .js-usersearch-input': (event, templateInstance) => {
-    if (event.keyCode === 13) {
-      event.preventDefault()
-      event.stopPropagation()
-      if ($('#tasks') && templateInstance.$('.js-usersearch-input').val()) {
-        templateInstance.$('.js-usersearch-results').addClass('d-none')
-        $('#tasks').focus()
-      }
-    }
-  },
-  'keyup .js-usersearch-input': (event, templateInstance) => {
-    if (event.keyCode === 40) {
-      event.preventDefault()
-      templateInstance.$('.js-usersearch-results').removeClass('d-none')
-      templateInstance.$(templateInstance.$('.js-usersearch-result')[0]).focus()
-    } else if (event.keyCode === 27) {
-      templateInstance.$('.js-usersearch-results').addClass('d-none')
-    } else {
-      templateInstance.$('.js-usersearch-results').removeClass('d-none')
-    }
-  },
-  'keyup .js-usersearch-result': (event, templateInstance) => {
-    event.preventDefault()
-    event.stopPropagation()
-    // enter key
-    if (event.keyCode === 13) {
-      templateInstance.$('.js-usersearch-input').val(templateInstance.$(event.currentTarget).children('.js-usersearch-user-name').text())
-      templateInstance.$('.js-usersearch-results').addClass('d-none')
-      if ($('#tasks')) {
-        $('#tasks').focus()
-      }
-    } else if ((event.keyCode === 40 || event.keyCode === 9) // tab or down key
-      && event.currentTarget.nextElementSibling) {
-      templateInstance.$(event.currentTarget.nextElementSibling).focus()
-    } else if (event.keyCode === 38 && event.currentTarget.previousElementSibling) { // up key
-      templateInstance.$(event.currentTarget.previousElementSibling).focus()
-    } else if (event.keyCode === 27) { // escape key
-      templateInstance.$('.js-usersearch-results').addClass('d-none')
-      templateInstance.$('.js-usersearch-input').focus()
-    }
+    templateInstance.filter.set('')
+    templateInstance.targetUser.renderIfNeeded()
   },
 })
 
@@ -109,24 +54,28 @@ Template.usersearch.onCreated(function usersearchcreated() {
     }
   })
 })
+Template.usersearch.onRendered(() => {
+  const templateInstance = Template.instance()
+  templateInstance.autorun(() => {
+    if (templateInstance.subscriptionsReady()) {
+      const users = templateInstance.users.get() || []
+      const userlist = users.map((entry) => ({ label: entry.profile.name, value: entry._id }))
+      if (!templateInstance.targetUser) {
+        templateInstance.targetUser = new Autocomplete(templateInstance.$('.js-usersearch-input').get(0), {
+          data: userlist,
+          maximumItems: getGlobalSetting('userSearchNumResults'),
+          threshold: 0,
+          onSelectItem: () => {
+            templateInstance.$('.js-usersearch-input').removeClass('is-invalid')
+            $('#tasks').first().trigger('focus')
+          },
+        })
+      } else if (userlist.length > 0) {
+        templateInstance.targetUser?.setData(userlist)
+      }
+    }
+  })
+})
 Template.usersearch.helpers({
-  getName: (user) => {
-    if (typeof user === 'string') {
-      const users = Template.instance().users.get()
-      if (!users) {
-        return ''
-      }
-      const found = users.find((u) => u._id === user)
-      if (found && found.profile) {
-        return found.profile.name
-      }
-    }
-    if (user.profile) {
-      return user.profile.name
-    }
-    return ''
-  },
   user: () => Template.instance()?.data?.user,
-  users: () => Template.instance().users.get(),
-  projectId: () => Template.instance()?.data?.projectId,
 })
