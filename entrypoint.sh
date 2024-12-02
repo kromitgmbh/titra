ae3d182e1966238eac760aba8ad7838c16dad6a9
Fabian Kromer <fabian.kromer@gmail.com>
2024-12-02 11:28:58 +0100

    🐳 updated docker image to be compatible with new async mongodb driver v6

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,18 @@ if [ -n "${MONGO_URL:-}" ]; then # Check for MongoDB connection if MONGO_URL is 
     cd bundle/programs/server/npm/node_modules/meteor/npm-mongo/node_modules
 	node <<- 'EOJS'
 	const mongoClient = require('mongodb').MongoClient;
-	setInterval(function() {
-		mongoClient.connect(process.env.MONGO_URL, function(err, client) {
-			if (client) {
-				console.log('Successfully connected to MongoDB');
-				client.close();
-			}
-			if (err) {
-				console.error(err);
-			} else {
-				process.exit(0);
-			}
-		});
+	setInterval(async function() {
+		let client;
+		try {
+			client = await mongoClient.connect(process.env.MONGO_URL);
+		} catch (err) {
+			console.error(err);
+		}
+		if (client && client.topology.isConnected()) {
+			console.log('Successfully connected to MongoDB');
+			client.close();
+			process.exit(0);
+		}
 	}, 1000);
 	EOJS
 fi
